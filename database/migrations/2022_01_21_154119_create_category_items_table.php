778aaaef4ba1adef33544d510c0ca8b64e08863b
S-Nojiri <the.ace.of.spades.ars@gmail.com>
2022-01-22 16:20:28 +0900

    migration作成

--- database/migrations/2022_01_21_154119_create_category_items_table.php
+++ database/migrations/2022_01_21_154119_create_category_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCategoryItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('category_items', function (Blueprint $table) {
             $table->increments('id')->index();
-            $table->string('name')->index();
-            $table->string('email')->unique()->index();
-            $table->string('password');
+            $table->string('category')->index();
+            $table->string('item')->index();
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category_items');
     }
 }
