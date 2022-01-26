1591076ff7c736dedcd419957947ede668895491
S-Nojiri <the.ace.of.spades.ars@gmail.com>
2022-01-26 15:04:16 +0900

    Seeder作成

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 class UserFactory extends Factory
 {
@@ -15,11 +16,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 
+            'created_at' => $this->faker->date($format='Y-m-d',$max='now'),
+            'updated_at' => $this->faker->dateTimeBetween($startDate = 'now', $endDate = '+2 week')->format('Y-m-d'), 
         ];
     }
 
